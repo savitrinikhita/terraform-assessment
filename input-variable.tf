f6e0e25f9a82236321da1a5309267fb979711c8c
savitrinikhita <48665144+savitrinikhita@users.noreply.github.com>
2023-01-05 00:59:46 +0530

    Terraform Assessment

--- input-variable.tf
+++ input-variable.tf
@@ -17,3 +17,33 @@ variable "resource_group_location" {
   type = string 
   default = "eastus"
 }
+
+variable "vnet_name" {
+  type = string
+  default = "vnet-default"
+}
+
+variable "vnet_address_space" {
+  type = list(string)
+  default = [ "10.0.0.0/16" ]
+}
+
+variable "web_subnet_name" {
+  type = string
+  default = "websubnet"
+}
+
+variable "web_subnet_address" {
+  type = list(string)
+  default = [ "10.0.1.0/24" ]
+}
+
+variable "app_subnet_name" {
+  type = string
+  default = "appsubnet"
+}
+
+variable "app_subnet_address" {
+  type = list(string)
+  default = [ "10.0.2.0/24" ]
+}
