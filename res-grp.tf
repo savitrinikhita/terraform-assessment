1bf921726ec7c1bee0e81d9c9a5ffc623806a0e2
savitrinikhita <48665144+savitrinikhita@users.noreply.github.com>
2023-01-05 01:02:11 +0530

    Terraform Assessment

--- res-grp.tf
+++ res-grp.tf
@@ -3,3 +3,10 @@ resource "azurerm_resource_group" "rg" {
   location = var.resource_group_location
   tags = local.common_tags
 }
+
+resource "random_string" "myrandom" {
+  length = 5
+  upper = false 
+  special = false
+  numeric = false
+}
